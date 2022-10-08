670d62d3e41072f00019d727f343c6c752d1261a
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-10-08 22:06:10 +0600

    signup page adding

---
+++ src/SkyWay/SkyWay.Shared/Pages/SignUpPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace SkyWay
+{
+    public sealed partial class SignUpPage : Page
+    {
+        #region Fields
+
+        private readonly IBackendService _backendService;
+
+        #endregion
+
+        #region Ctor
+
+        public SignUpPage()
+        {
+            this.InitializeComponent();
+            _backendService = (Application.Current as App).Host.Services.GetRequiredService<IBackendService>();
+        }
+
+        #endregion
+
+        #region Events
+
+        #region Page
+
+        #endregion
+
+        #region Buttons
+
+        private void GoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(typeof(StartPage));
+        }
+
+        #endregion
+
+        #region Input Fields
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Logic
+
+        #endregion
+
+        #region Page
+
+        private void NavigateToPage(Type pageType)
+        {
+            SoundHelper.PlaySound(SoundType.MENU_SELECT);
+            App.NavigateToPage(pageType);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
