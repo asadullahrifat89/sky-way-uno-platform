6c994eeffa25075866b2b51e26a7fc79a5c5dcd6
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-10-09 23:28:53 +0600

    adding how to play page

---
+++ src/SkyWay/SkyWay.Shared/Pages/HowToPlayPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+
+namespace SkyWay
+{
+    public sealed partial class HowToPlayPage : Page
+    {
+        #region Ctor
+
+        public HowToPlayPage()
+        {
+            this.InitializeComponent();
+        }
+
+        #endregion
+
+        #region Events
+
+        #region Page
+
+        #endregion
+
+        #region Buttons
+
+        private void GoBackButton_Click(object sender, RoutedEventArgs e)
+        {
+            NavigateToPage(typeof(StartPage));
+        }
+
+        #endregion
+
+        #endregion
+
+        #region Methods
+
+        #region Logic
+
+
+
+        #endregion
+
+        #region Page
+
+        private void NavigateToPage(Type pageType)
+        {
+            SoundHelper.PlaySound(SoundType.MENU_SELECT);
+            App.NavigateToPage(pageType);
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
