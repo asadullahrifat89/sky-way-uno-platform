8869faa01874dc05045f46587cc1c24c5e14e8d0
Asadullah Rifat <asadullah.rifat@selise.ch>
2022-10-10 01:45:09 +0600

    how to play page developed

--- src/SkyWay/SkyWay.Shared/Helpers/SoundHelper.cs
+++ src/SkyWay/SkyWay.Shared/Helpers/SoundHelper.cs
@@ -66,7 +66,7 @@ namespace SkyWay
             }
 
 #if DEBUG
-            Console.WriteLine("LOADED SOUDS: " + _playingSounds.Count);
+            Console.WriteLine("LOADED SOUNDS: " + _playingSounds.Count);
 #endif
         }
 
